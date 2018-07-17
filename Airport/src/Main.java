70ef1cf5fe3de40e441d61228b26085c908f8565
farazali7 <41275674+farazali7@users.noreply.github.com>
2018-07-17 01:15:42 -0400

    Add files via upload

--- Airport/src/Main.java
+++ Airport/src/Main.java
@@ -8,9 +8,7 @@ public class Main {
         Scanner reader = new Scanner(System.in);
         Airport airport = new Airport(reader);
         //Flights flight = new Flights();
-        
-        int[][] matrixA = {{1, 2, 3}, {2,5,4}};
-        
-        System.out.println(matrixA);
+                
+        airport.start();
     }
 }
